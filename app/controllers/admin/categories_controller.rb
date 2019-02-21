d7fdb938006edb82d4c874a996eb442f63a76737
Dylan Leard <dleard@uvic.ca>
2019-02-21 03:57:50 +0000

    add create category functionality

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,26 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+    @categories = Category.all()
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(params.require(:category).permit(:name))
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @category = Category.find params[:id]
+    @category.destroy
+    redirect_to [:admin, :categories], notice: 'Product deleted!'
+  end
+
+end
