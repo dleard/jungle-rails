8427e8d48ed125ddd9d705a8699bace28dcc2f09
Dylan Leard <dleard@uvic.ca>
2019-02-22 22:57:55 +0000

    set has_many and belongs_to for reviews, set reviews post route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,7 +12,6 @@ class ProductsController < ApplicationController
       user = User.find(review.user_id)
       review = {"user_id": review.user_id, "product_id": review.product_id, "description": review.description, "rating": review.rating, "created_at": review.created_at, "updated_at": review.updated_at, "user" => user.email}
       @reviews_with_user.push(review)
-      puts review.inspect
     end  
   end
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,6 @@ class Review < ActiveRecord::Base
   validates :product_id, :user_id, presence: true
   validates_presence_of :rating, :unless => :description?
   validates_presence_of :description, :unless => :rating?
+  belongs_to :product
+  belongs_to :user
 end
