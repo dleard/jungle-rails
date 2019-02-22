89fe6cac03514512356725b8d2385077e6c0bf04
Dylan Leard <dleard@uvic.ca>
2019-02-22 20:05:13 +0000

    fix some syntax errors

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ActiveRecord::Base
-  validates :product, :name, presence: true
+  validates :product_id, :user_id, presence: true
   validates_presence_of :rating, :unless => :description?
-  validates_presence_of :description :unless => rating?
+  validates_presence_of :description, :unless => :rating?
 end
