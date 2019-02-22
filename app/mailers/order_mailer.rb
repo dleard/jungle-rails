859cfd4fad53984ce709cf652c399a64b8e547ce
Dylan Leard <dleard@uvic.ca>
2019-02-22 01:25:49 +0000

    change email format to send html message

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,15 +3,9 @@ class OrderMailer < ApplicationMailer
   layout 'mailer'
 
   def send_email(order)
+    @order = order
     @email = order.email
-    @items = ''
-    order.line_items.each do |item|
-      @product = Product.find(item.product_id)
-      @items += " |  Name: " + @product.name.to_s + ", Qty: " + item.quantity.to_s + ", Price: " + @product.price.to_s + " | "
-    end
-    @items += "Total: " + (order.total_cents / 100.00).to_s
-    
-    mail(to: @email, from: 'noreply@jungle.com', subject: 'Order ID: ' + order.id.to_s, body: @items)
+    mail(to: @email, from: 'noreply@jungle.com', subject: 'Order ID: ' + order.id.to_s)
   end  
 
 end
