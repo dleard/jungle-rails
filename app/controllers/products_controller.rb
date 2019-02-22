502d7d45cb920b4ff30f12e891e2327845071b24
Dylan Leard <dleard@uvic.ca>
2019-02-22 22:18:55 +0000

    dynamically get review and user info for each review on a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,13 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find params[:id]
     @reviews = Review.where(product_id: @product.id)
+    @reviews_with_user = []
+    @reviews.each do |review|
+      user = User.find(review.user_id)
+      review = {"user_id": review.user_id, "product_id": review.product_id, "description": review.description, "rating": review.rating, "created_at": review.created_at, "updated_at": review.updated_at, "user" => user.email}
+      @reviews_with_user.push(review)
+      puts review.inspect
+    end  
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
       redirect_to '/signup'
     end
   end
+  
   private
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
