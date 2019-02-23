af03b0d12fab9f991c17177bf6529c072868af82
Dylan Leard <dleard@uvic.ca>
2019-02-23 02:18:19 +0000

    show reviews is reverse chronological order

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,7 +12,12 @@ class ProductsController < ApplicationController
       user = User.find(review.user_id)
       review = {"user_id": review.user_id, "product_id": review.product_id, "description": review.description, "rating": review.rating, "created_at": review.created_at, "updated_at": review.updated_at, "user" => user.email}
       @reviews_with_user.push(review)
-    end  
+    end
+    
+    # @reviews_with_user.sort {|a, b| a[:updated_at] <=> b[:updated_at]}
+    # @list.sort_by{|e| e[:time_ago]}
+    @reviews_with_user.sort_by{ |r|  r[:updated_at] }
+    @reviews_with_user.reverse!
   end
 
 end
