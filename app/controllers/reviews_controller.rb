1ae94338d1ace1fc2609591fc4233263cec3e27b
Dylan Leard <dleard@uvic.ca>
2019-02-23 01:15:16 +0000

    finish create method in reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,23 @@
 class ReviewsController < ApplicationController
-
+  
   def create
-    raise "Yo yo create that review"
+    @review = Review.new(review_params)
+    @review.user_id = session[:user_id]
+    @review.product_id = params[:product_id]
+    if @review.save
+      redirect_to '/'
+    else
+      redirect_to '/products/:product_id'
+    end  
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(
+      :product_id,
+      :user_id,
+      :description,
+      :rating
+    )
   end
 end
